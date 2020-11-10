c9b25c71bf9dddc970e0087655472aff28efaa58
Michael Walle <michael.walle@kontron.com>
2020-11-10 13:05:02 +0100

    update copyright
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- tsnic-flash.c
+++ tsnic-flash.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Kontron Europe GmbH
+ * Copyright (c) 2018-2020, Kontron Europe GmbH
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
