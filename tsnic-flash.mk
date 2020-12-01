0abf7247ad313ba42ee21961fbf96faca4966fff
Michael Walle <michael.walle@kontron.com>
2020-12-01 13:31:55 +0100

    compile static binary
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- tsnic-flash.mk
+++ tsnic-flash.mk
@@ -1,4 +1,4 @@
-ALL_TARGETS += $(o)tsnic-flash
+ALL_TARGETS += $(o)tsnic-flash $(o)tsnic-flash-static
 CLEAN_TARGETS += clean-tsnic-flash
 INSTALL_TARGETS += install-tsnic-flash
 
@@ -6,14 +6,20 @@ tsnic-flash_SOURCES := $(wildcard *.c)
 tsnic-flash_LIBS := -lpciaccess
 tsnic-flash_OBJECTS := $(addprefix $(o),$(tsnic-flash_SOURCES:.c=.o))
 
+tsnic-flash-static_LIBS := -lpciaccess
+tsnic-flash-static_LDFLAGS := -static
+
 $(o)%.o: %.c
 	$(call compile_tgt,tsnic-flash)
 
 $(o)tsnic-flash: $(tsnic-flash_OBJECTS)
 	$(call link_tgt,tsnic-flash)
 
+$(o)tsnic-flash-static: $(tsnic-flash_OBJECTS)
+	$(call link_tgt,tsnic-flash-static)
+
 clean-tsnic-flash:
-	rm -f $(tsnic-flash_OBJECTS) $(o)tsnic-flash
+	rm -f $(tsnic-flash_OBJECTS) $(o)tsnic-flash $(o)tsnic-flash-static
 
 install-tsnic-flash: $(o)tsnic-flash
 	$(INSTALL) -d -m 0755 $(DESTDIR)$(BINDIR)
